4e0c11b9525b7ab88549ee565a65809c7d147b48
All41n <allainescobar@outlook.com>
2021-02-05 22:17:15 +0000

    Dialog added to main page

--- components/Trendingcard.vue
+++ components/Trendingcard.vue
@@ -1,19 +1,35 @@
 <template>
-  <v-card class="ma-3 trending_card" height="auto" width="auto">
-    <v-img :src="imgURL + this.items.poster_path"></v-img>
-    <v-card-subtitle class="pb-0" id="movie_title">
-      {{
-        this.items.title ? this.items.title : this.items.name
-      }}</v-card-subtitle
+  <div>
+    <v-btn
+      height="auto"
+      width="auto"
+      class="modal_btn"
+      large
+      @click.stop="dialogInfo = true"
     >
-  </v-card>
+      <v-card class="ma-3 trending_card" height="auto" width="auto">
+        <v-img :src="imgURL + this.items.poster_path"></v-img>
+        <v-card-subtitle class="pb-0" id="movie_title">
+          {{
+            this.items.title ? this.items.title : this.items.name
+          }}</v-card-subtitle
+        >
+      </v-card>
+    </v-btn>
+    <Dialog :visible="dialogInfo" :items="items" @close="dialogInfo = false" />
+  </div>
 </template>
 
 <script>
+import Dialog from '../components/Dialog'
 export default {
+  components: {
+    Dialog,
+  },
   data() {
     return {
       imgURL: 'https://image.tmdb.org/t/p/w185_and_h278_bestv2',
+      dialogInfo: false,
     }
   },
   props: {
@@ -34,5 +50,17 @@ export default {
 #movie_title {
   font-size: 15px;
   color: #212121;
+}
+
+.theme--light.v-btn:not(.v-btn--flat):not(.v-btn--text):not(.v-btn--outlined){
+  background: none !important;
+}
+
+.v-btn--contained{
+  box-shadow: none !important;
+}
+
+.v-btn:not(.v-btn--round).v-size--large{
+  padding:0px;
 }
 </style>
